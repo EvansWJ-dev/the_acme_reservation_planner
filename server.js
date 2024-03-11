5f2397f9e2fe62d0cd9a5595eb8dc1e1a07fbe0b
unknown <williamevans001001@gmail.com>
2024-03-11 09:40:26 -0400

    added things to the server.js, to include the customers, restaurants, and reservations

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express;
 
 const customers = ["A", "B", "C", "D", "E", "F"];
+const restaurants = ["G", "H", "I", "J", "K", "L"];
+const reservation = ["M", "N", "O", "P", "Q", "R"];
 
 app.get("/api/customers", (req, res) => {
   res.json(customers);
@@ -16,6 +18,36 @@ app.push("/api/customers/:customer", (req, res) => {
   const { customer } = req.params;
   customers.push(customer);
   res.json(customer);
+});
+
+app.get("/api/restaurants", (req, res) => {
+  res.json(restaurants);
+});
+
+app.get("/api/restaurants/:restaurant", (req, res) => {
+  const { restaurant } = req.params;
+  res.json(restaurants.includes(restaurant));
+});
+
+app.push("/api/restaurants/:restaurant", (req, res) => {
+  const { restaurant } = req.params;
+  restaurants.push(restaurant);
+  res.json(restaurant);
+});
+
+app.get("/api/reservations", (req, res) => {
+  res.json(reservations);
+});
+
+app.get("/api/reservations/:reservation", (req, res) => {
+  const { reservation } = req.params;
+  res.json(reservations.includes(reservation));
+});
+
+app.push("/api/reservations/:reservation", (req, res) => {
+  const { reservation } = req.params;
+  reservations.push(reservation);
+  res.json(reservation);
 });
 
 app.listen(3000, () => {
